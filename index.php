98e430f1ed401460195d8ec4895b525f263ab240
Mochtar <mohmohtar56@gmail.com>
2023-07-13 01:21:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Watch Movies</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&family=Roboto&family=Roboto+Mono&family=Rubik:wght@300;400&display=swap" rel="stylesheet">
+  </head>
+  <style>
+    body{
+        font-family: 'Roboto', sans-serif;
+    }
+  </style>
+  <body>
+    <nav class="navbar bg-dark navbar-dark">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 fs-3 fw-bold ps-5">Movie App</span>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <h2 class="mt-4 mb-2 text-center">Search Movie</h2>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search Movie" aria-label="Recipient's username" aria-describedby="button-addon2" id="search-input">
+                    <button class="btn btn-dark" type="button" id="search-button">Search Movie</button>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row" id="movie-list">
+            
+        </div>
+    </div>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Movie Search</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" id="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="js/javascript.js"></script>
+  </body>
+</html>
